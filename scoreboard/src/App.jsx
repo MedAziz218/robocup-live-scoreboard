3bd065f8e8c719cf01cd742d1671150b2c0d8665
Mohamed Aziz Lahdheri <118969900+MedAziz218@users.noreply.github.com>
2023-10-14 16:44:47 +0100

    added ZoomTestPage to test react-zoomable-ui lib

--- scoreboard/src/App.jsx
+++ scoreboard/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import { ZoomTestPage } from './pages/zoomtestPage/ZoomTestPage'
+import { suppressBrowserZooming } from 'react-zoomable-ui';
+import { useEffect } from 'react';
 function App() {
+  useEffect(suppressBrowserZooming);
 
   return (
     <>
-      
+    <div >
+
+      <ZoomTestPage/>
+    </div>
         
     </>
   )
