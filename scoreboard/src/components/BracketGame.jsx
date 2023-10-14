1b8790912eb5b44511b4264bd3530d729596fcb7
riahifiras <120566908+riahifiras@users.noreply.github.com>
2023-10-14 14:50:56 +0100

    Setup tailwindcss.

---
+++ scoreboard/src/components/BracketGame.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BracketGame = () => {
+    const name = "firas";
+    return (
+        <div className='bg-blue-300 text-black'>
+            {name}
+        </div>
+    )
+}
+
+export default BracketGame
